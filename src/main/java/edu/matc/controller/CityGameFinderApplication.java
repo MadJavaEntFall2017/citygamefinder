de354b727b62ef2639f5c2c039cd16bc1f1a1116
Layton Emerson <lemerson1@madisoncollege.edu>
2017-11-04 13:40:07 -0500

    Shelling out URI's within the service

---
+++ src/main/java/edu/matc/controller/CityGameFinderApplication.java
@@ -0,0 +1,21 @@
+package edu.matc.controller;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+//Defines the base URI for all resource URIs.
+@ApplicationPath("/")
+
+//The java class declares root resource and provider classes
+public class CityGameFinderApplication extends Application {
+
+    //The method returns a non-empty collection with classes, that must be included in the published JAX-RS application
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet h = new HashSet<Class<?>>();
+        h.add(SportsService.class );
+        return h;
+    }
+}
---
+++ src/main/java/edu/matc/controller/SportsService.java
@@ -0,0 +1,100 @@
+package edu.matc.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/games")
+public class SportsService {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getMessage() throws Exception {
+
+        String outputString = "You want the full schedule for all 4 sports";
+
+        ObjectMapper mapper = new ObjectMapper();
+        String output = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(outputString);
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{sport}")
+    public Response getMessage(@PathParam("sport") String sport)  throws Exception{
+
+        String outputString = "You want the full schedule for " + sport;
+
+        ObjectMapper mapper = new ObjectMapper();
+        String output = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(outputString);
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{zip}/{radius}")
+    public Response getMessage(@PathParam("zip") String zipCode,
+                               @PathParam("radius") String radius)  throws Exception{
+
+        String outputString = "You want the games of all sports within " + radius + " miles of " + zipCode;
+
+        ObjectMapper mapper = new ObjectMapper();
+        String output = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(outputString);
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{sport}/{zip}/{radius}")
+    public Response getMessage(@PathParam("sport") String sport,
+                               @PathParam("zip") String zipCode,
+                               @PathParam("radius") String radius)  throws Exception{
+
+        String outputString = "You want the " + sport + " games within " + radius + " miles of " + zipCode;
+
+        ObjectMapper mapper = new ObjectMapper();
+        String output = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(outputString);
+        return Response.status(200).entity(output).build();
+    }
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{sport}/{zip}/{radius}/{fromDate}")
+    public Response getMessage(@PathParam("sport") String sport,
+                               @PathParam("zip") String zipCode,
+                               @PathParam("radius") String radius,
+                               @PathParam("fromDate") String fromDate)  throws Exception{
+
+        String outputString = "You want the " + sport + " games within " + radius + " miles of " + zipCode +
+                " on or after " + fromDate;
+
+        ObjectMapper mapper = new ObjectMapper();
+        String output = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(outputString);
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{sport}/{zip}/{radius}/{fromDate}/{toDate}")
+    public Response getMessage(@PathParam("sport") String sport,
+                               @PathParam("zip") String zipCode,
+                               @PathParam("radius") String radius,
+                               @PathParam("fromDate") String fromDate,
+                               @PathParam("toDate") String toDate)  throws Exception{
+
+        String outputString = "You want the " + sport + " games within " + radius + " miles of " + zipCode +
+                " between the dates of " + fromDate + " & " + toDate;
+
+        ObjectMapper mapper = new ObjectMapper();
+        String output = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(outputString);
+        return Response.status(200).entity(output).build();
+    }
+
+
+}
