d749aee1bebe256e3bfd3265850384faa7e92037
Jamie Kruser <jmkruser@madisoncollege.edu>
2017-11-13 00:09:19 -0600

    Fixed up a few of the tests

--- src/test/java/edu/matc/controller/SportsServiceTest.java
+++ src/test/java/edu/matc/controller/SportsServiceTest.java
@@ -19,14 +19,14 @@ public class SportsServiceTest {
     public void getMessageTest() throws Exception {
         Response response = sportsService.getMessage();
 
-        assertEquals(true, response.hasEntity());
+        assertEquals(200, response.getStatus());
     }
 
     @Test
     public void getMessage1Test() throws Exception {
         Response response = sportsService.getMessage("NFL");
 
-        assertEquals(true, response.hasEntity());
+        assertEquals(200, response.getStatus());
     }
 
     @Test
@@ -41,28 +41,28 @@ public class SportsServiceTest {
     public void getMessage2Test() throws Exception {
         Response response = sportsService.getMessage("53718", "200");
 
-        assertEquals(true, response.hasEntity());
+        assertEquals(200, response.getStatus());
     }
 
     @Test
     public void getMessage3Test() throws Exception {
         Response response = sportsService.getMessage("NFL", "53718", "200");
 
-        assertEquals(true, response.hasEntity());
+        assertEquals(200, response.getStatus());
     }
 
     @Test
     public void getMessage4Test() throws Exception {
         Response response = sportsService.getMessage("NFL", "53718", "200", "2017-12-01");
 
-        assertEquals(true, response.hasEntity());
+        assertEquals(200, response.getStatus());
     }
 
     @Test
     public void getMessage5Test() throws Exception {
         Response response = sportsService.getMessage("NFL", "53718", "200", "2017-12-01", "2017-12-31");
 
-        assertEquals(true, response.hasEntity());
+        assertEquals(200, response.getStatus());
     }
 
 }
